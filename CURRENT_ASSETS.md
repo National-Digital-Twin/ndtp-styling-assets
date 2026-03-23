f199142d36d05217d1dbba89dbf26449c171d674
Sam Hill <sam.hill@informed.com>
2026-03-23 14:59:02 +0000

    feat: initial OSS release/0.90.0

---
+++ CURRENT_ASSETS.md
@@ -0,0 +1,47 @@
+# Current Assets
+
+**Repository:** `ndtp-styling-assets`  
+**Description:** `Contains a list of existing assets served by the package`
+
+## NDTP Colors
+
+- Primary color
+- Secondary color
+- Accent Yellow
+- Deep Navy
+- Off white
+
+## NDTP typography
+
+- font family
+
+## NDTP Components
+
+### Button
+
+- Button border radius
+- Button text transform
+
+### Toolbar
+
+- Toolbar height
+- Logo height
+- Toolbar font color
+- Toolbar container class
+- Toolbar left side class
+- Toolbar right side class
+
+## NDTP Logos
+
+- IRIS NDTP logo
+- LISA NDTP logo
+- Paralog NDTP logo
+
+## NDTP Favicon
+
+- NDTP favicon
+
+© Crown Copyright 2025. This work has been developed by the National Digital Twin Programme and is legally attributed to the Department for Business and Trade (UK) as the governing entity.
+
+Licensed under the Open Government Licence v3.0.
+For full licensing terms, see [OGL_LICENSE.md](OGL_LICENSE.md).
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: Apache-2.0
+// © Crown Copyright 2025. This work has been developed by the National Digital Twin Programme and is legally attributed to the Department for Business and Trade (UK) as the governing entity.
+
+import path from 'path';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import CopyPlugin from 'copy-webpack-plugin';
+
+export default {
+  entry: './src/scss/main.scss',  // SCSS entry file
+  output: {
+    path: path.resolve('dist'),
+    filename: 'bundle.js',  // Not used directly for SCSS, but necessary for Webpack setup
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,  // Extract CSS from SCSS
+          'css-loader',  // CSS Loader to handle imports of CSS
+          'sass-loader',  // Compile SCSS into CSS
+        ],
+      },
+    ],
+  },
+  plugins: [
+    // Extract CSS into a separate file
+    new MiniCssExtractPlugin({
+      filename: 'css/styles.css',  // Output the compiled CSS to this file
+    }),
+
+    // Copy SCSS files from src/scss/ into dist/scss/ maintaining folder structure (but not duplicating src/scss)
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'src/scss',  // Match all files in src/scss and subdirectories
+          to: 'scss',  // Copy directly to dist/scss, no additional folder structure
+        },
+        {
+          from: 'src/assets',  // Match all files in src/scss and subdirectories
+          to: 'assets',  // Copy directly to dist/scss, no additional folder structure
+        },
+      ],
+    }),
+  ],
+  mode: 'production',
+};
